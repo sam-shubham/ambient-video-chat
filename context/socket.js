7f7a4fece81aa65ce4ba4a44da98a5d8444286a8
Sam Shubham <68236217+sam-shubham@users.noreply.github.com>
2025-04-04 00:23:28 +0530

    added express socket

--- context/socket.js
+++ context/socket.js
@@ -4,24 +4,24 @@ import { io } from "socket.io-client";
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-    const socket = useContext(SocketContext)
-    return socket
-}
+  const socket = useContext(SocketContext);
+  return socket;
+};
 
 export const SocketProvider = (props) => {
   const { children } = props;
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const connection = io();
-    console.log("socket connection", connection)
+    const connection = io("http://localhost:4000");
+    console.log("socket connection", connection);
     setSocket(connection);
   }, []);
 
-  socket?.on('connect_error', async (err) => {
-    console.log("Error establishing socket", err)
-    await fetch('/api/socket')
-  })
+  socket?.on("connect_error", async (err) => {
+    console.log("Error establishing socket", err);
+    await fetch("http://localhost:4000/");
+  });
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
